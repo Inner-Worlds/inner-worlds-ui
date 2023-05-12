8d2d43624f0b97953cc3a6e96baf5c0ace8ff433
Lauren Seward <lauren.unal@gmail.com>
2023-05-12 18:29:25 -0500

    Add basic dream input form functionality

--- src/Components/DreamInput/DreamInput.js
+++ src/Components/DreamInput/DreamInput.js
@@ -1,9 +1,111 @@
-import "./DreamInput.css"
+import { useState } from 'react';
+import { mockEmotions, mockTags } from '../../mock-data';
 
 const DreamInput = () => {
-    return (
-        <></>
-    )
-}
+  const [date, setDate] = useState('');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [selectedEmotion, setSelectedEmotion] = useState('');
+  const [selectedTag, setSelectedTag] = useState('');
+  const [lucidityLevel, setLucidityLevel] = useState(0);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const dreamData = {
+      date,
+      label: title,
+      description,
+      emotions: [selectedEmotion],
+      tags: [selectedTag],
+      lucidityLevel,
+    };
+
+    console.log(dreamData);
+
+    setDate('');
+    setTitle('');
+    setDescription('');
+    setSelectedEmotion('');
+    setSelectedTag('');
+    setLucidityLevel(0);
+  };
+
+  return (
+    <div className="dream-input">
+      <h2>Record Your Dream</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Date:
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Title:
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Description:
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Emotion:
+          <select
+            value={selectedEmotion}
+            onChange={(e) => setSelectedEmotion(e.target.value)}
+          >
+            <option value="">Select an emotion</option>
+            {mockEmotions.data.emotions.map((emotion) => (
+              <option key={emotion} value={emotion}>
+                {emotion}
+              </option>
+            ))}
+          </select>
+        </label>
+        <br />
+        <label>
+          Tag:
+          <select
+            value={selectedTag}
+            onChange={(e) => setSelectedTag(e.target.value)}
+          >
+            <option value="">Select a tag</option>
+            {mockTags.data.tags.map((tag) => (
+              <option key={tag} value={tag}>
+                {tag}
+              </option>
+            ))}
+          </select>
+        </label>
+        <br />
+        <label>
+          Lucidity Level: {lucidityLevel}
+          <input
+            type="range"
+            min={0}
+            max={5}
+            value={lucidityLevel}
+            onChange={(e) => setLucidityLevel(Number(e.target.value))}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default DreamInput;
