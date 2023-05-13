5b4a8b4858e99bda19b94206a9717734710078f9
Alexys Jordan <alexyskjordan@gmail.com>
2023-05-13 11:19:53 -0600

    Add Nav styling

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,14 +5,20 @@ import Login from '../Login/Login';
 import DreamInput from '../DreamInput/DreamInput';
 import DreamList from '../DreamList/DreamList';
 import NotFound from '../NotFound/NotFound';
+import Nav from '../Nav/Nav';
 
 const App = () => {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" render={() => <Login /> } />
-        <Route exact path="/Home" render={() => <DreamInput />} />
-        <Route exact path="/Dreams" render={() => <DreamList />} />
+        <Route exact path="/Home" render={() => 
+        <>
+        <DreamInput />
+        <Nav />
+        </>} />
+        <Route exact path="/Dreams" render={() => <DreamList />
+      } />
         <Route path="*" render={() => <NotFound />} />
       </Switch>
     </div>
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,23 +1,23 @@
-// import React from "react";
-// import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
-// import "./Nav.css" 
+import React from "react";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import "./Nav.css" 
 
-// const Nav = () => {
-//     return (
-//         <header className="nav-bar">
-//             <NavLink to="/Home">
-//             <img className="logo" />  
-//             </NavLink>
-//         <div className="nav-links">
-//             <NavLink to="/Home" className='nav-link1' >
-//                 Home
-//             </NavLink>
-//             <NavLink to="/Dreams" className='nav-link2'>
-//                 My Dreams
-//             </NavLink>
-//         </div>
-//     </header>
-//     )
-// }
+const Nav = () => {
+    return (
+        <header className="nav-bar">
+            <NavLink to="/Home">
+            <img className="logo" />  
+            </NavLink>
+        <div className="nav-links">
+            <NavLink to="/Home" className='nav-link1' >
+                Home
+            </NavLink>
+            <NavLink to="/Dreams" className='nav-link2'>
+                My Dreams
+            </NavLink>
+        </div>
+    </header>
+    )
+}
 
-// export default Nav;
+export default Nav;
