081374bcc4595444051b7c764d0f57f39063df61
Daniel Curtin <dcurtin.work@gmail.com>
2023-05-15 15:50:51 -0600

    Add dream cards and use carousel

--- src/Components/DreamList/DreamList.js
+++ src/Components/DreamList/DreamList.js
@@ -1,9 +1,20 @@
 import "./DreamList.css"
+import DreamCard from "../DreamCard/DreamCard";
+import { useCarousel } from "use-carousel-hook";
 
-const DreamList = () => {
+const DreamList = ({ dreams }) => {
+    const { ref, previous, next, setCurrent, reset } = useCarousel();
+
+    const dreamCards = dreams.map(dream => <DreamCard key={dream.id} id={dream.id} date={dream.date} title={dream.title} description={dream.description} emotions={dream.emotions} tags={dream.tags} lucidity={dream.lucidityLevel}/>)
+    
     return (
-        <>
-        </>
+        <main className="dream-list">
+            <button className="carousel-button prev fa-solid fa-chevron-left" onClick={() => previous()}></button>
+            <ul ref={ref} className="carousel__list dream-carousel">
+                {dreamCards}
+            </ul>
+            <button className="carousel-button next fa-solid fa-chevron-right" onClick={() => next()}></button>
+        </main>
     )
 }
 
