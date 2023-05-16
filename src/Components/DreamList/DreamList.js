cdbd6b78534ff4aa47c761d564fc82fd734ad139
Daniel Curtin <dcurtin.work@gmail.com>
2023-05-16 13:08:48 -0600

    Remove use-carousel-hook references

--- src/Components/DreamList/DreamList.js
+++ src/Components/DreamList/DreamList.js
@@ -1,19 +1,16 @@
 import "./DreamList.css"
 import DreamCard from "../DreamCard/DreamCard";
-import { useCarousel } from "use-carousel-hook";
 
 const DreamList = ({ dreams }) => {
-    const { ref, previous, next } = useCarousel();
-
     const dreamCards = dreams.map(dream => <DreamCard key={dream.id} id={dream.id} date={dream.date} title={dream.title} description={dream.description} emotions={dream.emotions} tags={dream.tags} lucidity={dream.lucidityLevel}/>)
     
     return (
         <main className="dream-list">
-            <button className="carousel-button prev fa-solid fa-chevron-left" onClick={() => previous()}></button>
-            <ul ref={ref} className="carousel__list dream-carousel">
+            <button className="carousel-button prev fa-solid fa-chevron-left"></button>
+            <ul className="carousel__list dream-carousel">
                 {dreamCards}
             </ul>
-            <button className="carousel-button next fa-solid fa-chevron-right" onClick={() => next()}></button>
+            <button className="carousel-button next fa-solid fa-chevron-right"></button>
         </main>
     )
 }
