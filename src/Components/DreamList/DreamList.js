abc4d79a0d96b0ca3614ee40dd5ef131eec0d522
Daniel Curtin <dcurtin.work@gmail.com>
2023-05-16 15:32:49 -0600

    Refactor dream list organization

--- src/Components/DreamList/DreamList.js
+++ src/Components/DreamList/DreamList.js
@@ -4,27 +4,35 @@ import DreamCard from "../DreamCard/DreamCard";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
 import "swiper/css";
-// import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 const DreamList = ({ dreams }) => {
-    const dreamCards = dreams.map(dream => <SwiperSlide key={dream.id}><DreamCard id={dream.id} date={dream.date} title={dream.title} description={dream.description} emotions={dream.emotions} tags={dream.tags} lucidity={dream.lucidityLevel}/></SwiperSlide>)
+    const dreamCards = dreams.map(dream => {
+        return <SwiperSlide key={dream.id}>
+                    <DreamCard 
+                        id={dream.id} 
+                        date={dream.date} 
+                        title={dream.title} 
+                        description={dream.description} 
+                        emotions={dream.emotions} 
+                        tags={dream.tags} 
+                        lucidity={dream.lucidityLevel}
+                    />
+                </SwiperSlide>
+    });
     
     return (
         <>
-            {/* <button className="carousel-button prev fa-solid fa-chevron-left"></button> */}
             <Swiper 
                 slidesPerView={3}
                 spaceBetween={30}
                 centeredSlides={true}
-                // pagination={{ clickable: true }}
                 navigation={true}
                 modules={[Navigation]}
                 className="mySwiper"
             >
                 {dreamCards}
             </Swiper>
-            {/* <button className="carousel-button next fa-solid fa-chevron-right"></button> */}
         </>
     )
 }
