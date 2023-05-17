0ff1420441f1fa3bd3bbfd592e771275ecea781a
Daniel Curtin <dcurtin.work@gmail.com>
2023-05-17 12:40:40 -0600

    Fix dream card generation with new backend data/schema

--- src/Components/DreamCard/DreamCard.js
+++ src/Components/DreamCard/DreamCard.js
@@ -7,9 +7,9 @@ const DreamCard = ({ id, date, title, description, emotions, tags, lucidity }) =
       <p className="date">{date}</p>
       <h1 className="title">{title}</h1>
       <p className="description">{description}</p>
-      <ul className="emotions-list">{emotions.map(emotion => <li key={emotion}>{emotion}</li>)}</ul>
+      <ul className="emotions-list">{emotions.map(emotion => <li key={`emotion${emotion.id}`} data-emotionid={emotion.id}>{emotion.name}</li>)}</ul>
       <div className="styling separator"></div>
-      <ul className="tags-list">{tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+      <ul className="tags-list">{tags.map(tag => <li key={`tag${tag.id}`} data-tagid={tag.id}>{tag.name}</li>)}</ul>
       <h3 className="lucidity">Lucidity Level: {lucidity}</h3>
       <div className="dream-buttons">
         <button className="edit-dream-button fa-solid fa-pen-to-square"></button>
