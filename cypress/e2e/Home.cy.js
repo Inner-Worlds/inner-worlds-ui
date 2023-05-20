101a5db270178950c5c4a9864c963df1a5a7c3ee
Alexys Jordan <alexyskjordan@gmail.com>
2023-05-20 10:56:17 -0600

    fix dream typo

--- cypress/e2e/Home.cy.js
+++ cypress/e2e/Home.cy.js
@@ -5,6 +5,7 @@ describe('Home page', () => {
     });
     cy.visit('http://localhost:3000/home');
   });
+
   it('should display the logo and navigate to Home when clicked', () => {
     cy.get('.logo').click();
     cy.url().should('include', '/Home');
@@ -26,14 +27,13 @@ describe('Home page', () => {
       fixture: "dreamFixture"
     });
     cy.get('.nav-link2').click();
-    cy.url()
+    cy.url().should('include', '/Dreams');
   });
 
   it('should navigate to Login page and trigger Log Out when Log Out link is clicked', () => {
     cy.get('.nav-link3').click();
     cy.url().should('include', '/');
   });
-
 
   it('should see a form to enter the details of their dreams', () => {
     cy.get('h2').contains('Dream Journal');
@@ -49,7 +49,6 @@ describe('Home page', () => {
   it('should be able to select a date and type a title and description of the dream', () => {
     cy.get('[type="date"]').click().then(() => {
       cy.get('[type="date"]').type('2023-01-07');
-
     });
     cy.get('[placeholder="My Dream Title.."]').type('Prince Humperdinck');
     cy.get('textarea').type('Ray has gone bye-bye');
@@ -65,20 +64,20 @@ describe('Home page', () => {
 
   it('should be able to submit their dream', () => {
     cy.get("button[type='submit']").click();
-  })
+  });
+
   it('should handle dream submission error', () => {
     cy.intercept('POST', '/graphql', (req) => {
       req.reply({
         statusCode: 500,
         body: {
-          errors: [{ message: 'Please fill out this feild' }]
+          errors: [{ message: 'Please fill out this field' }]
         }
       });
     });
-
     
     cy.get('[type="date"]').click().then(() => {
-      cy.get('[type="date"]')
+      cy.get('[type="date"]');
       cy.get('[placeholder="My Dream Title.."]').type('Prince Humperdinck');
       cy.get('textarea').type('Ray has gone bye-bye');
       cy.get(".multi-select").eq(0).click();
@@ -87,9 +86,6 @@ describe('Home page', () => {
       cy.get(".select-styling__option").contains('Work').click();
       cy.get('input[type="range"]').invoke("val", 4).trigger("change");
       cy.get("button[type='submit']").click();
-  
+    });
   });
-  });
-  });
-  
-
+});
